69e1199d9f338b0f3685f71c3ec7d8ef11d84cdb
vijaykarthik <nvijaykarthik@gmail.com>
2020-01-15 11:32:18 +0530

    updated admin screen

--- src/main/resources/static/admin/service-worker.js
+++ src/main/resources/static/admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/admin/precache-manifest.74c9b1c72ba9af9f2581540453c89f58.js"
+  "/admin/precache-manifest.fd7ae739bd6e5d8750838e6ef6795338.js"
 );
 
 self.addEventListener('message', (event) => {
